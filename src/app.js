29291b2accd4cb082ffc67db358d1f1e128358d2
natkaulaP <89267067+natkaulaP@users.noreply.github.com>
2021-09-30 11:21:44 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -42,28 +42,30 @@ searchForm.addEventListener("submit", fetchCity);
 
 function fetchWeather(response) {
   let city = document.querySelector("#city");
-  city.innerHTML = response.data.name;
   let temperature = Math.round(response.data.main.temp);
   let description = document.querySelector("#description");
-  description.innerHTML = response.data.weather[0].main;
   let temp = document.querySelector("#temp");
-  temp.innerHTML = `${temperature}°C`;
   let pressure = document.querySelector("#pressure");
-  pressure.innerHTML = `Pressure: ${response.data.main.pressure} mb`;
   let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = `Humidity ${response.data.main.humidity} %`;
-  let wind = Math.round(response.data.wind.speed);
+   let wind = Math.round(response.data.wind.speed);
   let windSpeed = document.querySelector("#wind");
-  windSpeed.innerHTML = `Wind speed ${wind} km/h`;
   let date = document.querySelector("#date-and-time");
-  date.innerHTML = formatDate(response.data.dt * 1000);
  let icon =document.querySelector("#icon");
- icon.setAttribute(
+
+celsiusTemp =response.data.main.temp;
+ temp.innerHTML = `${temperature}°C`;
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].main;
+  pressure.innerHTML = `Pressure: ${response.data.main.pressure} mb`;
+  humidity.innerHTML = `Humidity ${response.data.main.humidity} %`;
+windSpeed.innerHTML = `Wind speed ${wind} km/h`;
+  date.innerHTML = formatDate(response.data.dt * 1000);
+  icon.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", response.data.weather[0].description);
-  celsiusTemp =response.data.main.temp;
+  
 }
 
 function locateUser(position) {
